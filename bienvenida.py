d3d06fac5c5f302437fd4f7f353eecbcdc926c98
a01736397-coder <a01736397@tec.mx>
2025-10-01 18:44:27 -0600

    creacion funciones front

--- bienvenida.py
+++ bienvenida.py
@@ -6,32 +6,21 @@ def suma(a,b):
 
 print(suma(4,6))
 
-def generar_datos_ventas():
-    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
-    ventas = [120, 500, 230, 289, 310]
-    return dias, ventas
-
-def resumen_analitica():
-    dias, ventas = generar_datos_ventas()
-    total = sum(ventas)
-    promedio = total/len(ventas)
-    return {
-        "dias": dias,
-        "ventas": ventas,
-        "total": total,
-        "promedio": promedio
-    }
-datos = resumen_analitica()
-print("Total de ventas :", datos["total"])
-print("promedio diario : ", datos["promedio"])
 
 import matplotlib.pyplot as plt
 
 def graficar_ventas(dias, ventas):
     plt.figure(figsize=(8,4))
-    plt.plot(dias, ventas, markers = "o", linestyle ="-", color ="red")
+    plt.plot(dias, ventas, marker = "o", linestyle ="-", color ="red")
     plt.title("Ventas diarias")
     plt.xlabel("Día")
     plt.ylabel("Ventas")
     plt.grid(True)
-    plt.show 
+    plt.show()
+
+def main_frontend(): 
+    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
+    ventas = [120, 500, 230, 289, 310]
+    graficar_ventas(dias,ventas)
+
+main_frontend()
