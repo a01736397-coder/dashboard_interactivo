875c3a58e9c36e8880482585035aff11651795bc
a01736397-coder <a01736397@tec.mx>
2025-10-02 18:31:50 -0600

    back

--- bienvenida.py
+++ bienvenida.py
@@ -1,7 +1,51 @@
-print("hola de nuevo")
-print("lp")
+print("")
+print("Hola de nuevo")
 
-def suma(a,b):
-    return a+b
+a = 40
+b = 30
+c = 8
 
-print(suma(4,6))
+def suma():
+    print("Suma total: ", a + b + c)
+
+suma()
+
+
+import matplotlib.pyplot as plt
+
+def graficar_ventas(dias, ventas):
+    plt.figure(figsize=(8,4))
+    plt.plot(dias, ventas, marker = "o", linestyle ="-", color ="red")
+    plt.title("Ventas diarias")
+    plt.xlabel("Día")
+    plt.ylabel("Ventas")
+    plt.grid(True)
+    plt.show()
+
+def main_frontend(): 
+    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
+    ventas = [120, 500, 230, 289, 310]
+    graficar_ventas(dias, ventas)
+
+main_frontend()
+
+print("Despliegue de datos")
+
+def generar_datos_ventas():
+    dias = ["Lun", "Mar", "Mie", "Jue", "Vie"]
+    ventas = [120, 500, 230, 289, 310]
+    return dias, ventas
+
+def resumen_analitica():
+    dias, ventas = generar_datos_ventas()
+    total = sum(ventas)
+    promedio = total / len(ventas)
+    return {
+        "dias": dias, 
+        "ventas": ventas,
+        "total": total, 
+        "promedio": promedio
+    }
+datos = resumen_analitica()
+print("Total de ventas :", datos["total"])
+print("Promedio diario:", datos["promedio"])
